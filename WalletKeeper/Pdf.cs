04087f894fefe01ac9ad0d20ad3c568ede7298cb
cyrilq <kuhar4uk282@gmail.com>
2016-12-07 14:23:22 +0300

    Basic pdf generation added

---
+++ WalletKeeper/Pdf.cs
@@ -0,0 +1,33 @@
+﻿using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletKeeper
+{
+    class Pdf
+    {
+        public static void GeneratePDF()
+        {
+            //create a document object
+            var doc = new Document();
+            //get the current directory
+            string path = Environment.CurrentDirectory;
+            //get PdfWriter object
+            PdfWriter.GetInstance(doc, new FileStream(path + "/pdfdoc.pdf", FileMode.Create));
+            //open the document for writing
+            doc.Open();
+            //write a paragraph to the document
+            doc.Add(new Paragraph("Hello World"));
+            //close the document
+            doc.Close();
+            //view the result pdf file
+            System.Diagnostics.Process.Start(path + "/pdfdoc.pdf");
+        }
+    }
+}
--- WalletKeeper/Program.cs
+++ WalletKeeper/Program.cs
@@ -1,26 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.InlineQueryResults;
-using Telegram.Bot.Types.InputMessageContents;
-using Telegram.Bot.Types.ReplyMarkups;
 
 namespace WalletKeeperBot
 {
     class Program
     {
+
         private static readonly TelegramBotClient Bot = new TelegramBotClient("293942981:AAECoeXWb-5WJY1Az8JPyvUoaZ-du2-tuCc");
+
         static void Main(string[] args)
         {
             Bot.OnMessage += BotOnMessageReceived;
             Bot.OnMessage += BotOnPhotoReceived;
+
+            WalletKeeper.Pdf.GeneratePDF();
 
             var me = Bot.GetMeAsync().Result;
             Console.Title = me.Username;
@@ -33,6 +31,7 @@ namespace WalletKeeperBot
         private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
         {
             var message = messageEventArgs.Message;
+            
 
             if (message == null || message.Type != MessageType.TextMessage) return;
 
@@ -61,6 +60,7 @@ namespace WalletKeeperBot
             const string weVeGotYourPhoto = "He have got your photo! Congratz!";
 
             await Bot.SendTextMessageAsync(message.Chat.Id, weVeGotYourPhoto);
+            //await Bot.SendDocumentAsync(message.Chat.Id, )
         }
 
     }
