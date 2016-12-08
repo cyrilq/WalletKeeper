eb1437446d71d70941b5a52713618fb4ee82f387
LevashovaPolina <polina-levashova@yandex.ru>
2016-12-08 22:17:46 +0300

    Added TrialFile

---
+++ WalletKeeper/TrialFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletKeeper
+{
+    class TrialFile
+    {
+        string API_KEY = "293942981:AAECoeXWb-5WJY1Az8JPyvUoaZ-du2-tuCc";
+    }
+}
